6abab2d80e60ea9a032b2a4617da240b1cf4d83d
Navneet Yadav <nicenavneet@gmail.com>
2026-04-24 15:40:14 +0530

    Initial Commit

---
+++ IOS Headunit/IOS_HeadunitApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_HeadunitApp.swift
+//  IOS Headunit
+//
+//  Created by Navneet Yadav on 24/04/26.
+//
+
+import SwiftUI
+
+@main
+struct IOS_HeadunitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
